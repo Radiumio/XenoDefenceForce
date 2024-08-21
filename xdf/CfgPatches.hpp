9c887d046a32f67b8aadb314abfc7505a9c7154b
Radiumio <shaianmacleod@gmail.com>
2024-08-21 23:00:05 +0200

    Fix Zeus compatibility

--- xdf/CfgPatches.hpp
+++ xdf/CfgPatches.hpp
@@ -6,7 +6,22 @@ class CfgPatches
 		version = "1.0";
 		author="Radium";
 		authorUrl = "https://steamcommunity.com/id/radiumio/";
-		units[] = {};
+		units[] = 
+		{
+			"XDF_Gemini",
+			"XDF_Caiman",
+			"XDF_Skyhawk",
+			"XDF_Mars",
+			"XDF_Hornet",
+			"XDF_Boar",
+			"XDF_Boar_GMG",
+			"XDF_Boar_HMG",
+			"XDF_Jackal",
+			"XDF_Puma",
+			"XDF_Puma_GMG",
+			"XDF_Puma_HMG",
+			"XDF_Odin"
+		};
 		weapons[] = {};
 	};
 
