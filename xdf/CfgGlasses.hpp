4abf3382903c68575f9bd2870e03465b2135169f
Radiumio <shaianmacleod@gmail.com>
2024-10-09 00:20:04 +0200

    Added XDF CBRN respirators

--- xdf/CfgGlasses.hpp
+++ xdf/CfgGlasses.hpp
@@ -3,6 +3,8 @@ class CfgGlasses
 	class G_SSU_Balaclava_TI_base;
 	class G_SSU_Balaclava_TI_G_base;
 	class G_Combat;
+	class G_AirPurifyingRespirator_01_F;
+	class G_AirPurifyingRespirator_02_black_F;
 	class G_XDF_Balaclava_TI_Black: G_SSU_Balaclava_TI_base
 	{
 		author="Clasless & Radium";
@@ -46,6 +48,76 @@ class CfgGlasses
 		hiddenSelectionsTextures[]=
 		{
 			"\xdf\retextures\xdf_uniform\facegear\xdf_combat_goggles_black.paa"
+		};
+	};
+	class G_XDF_APR_A: G_AirPurifyingRespirator_01_F
+	{
+		author="Radium";
+		scope=2;
+		scopeArsenal=2;
+		scopeCurator=2;
+		displayName="XDF APR Alpha";
+		picture="\A3\Characters_F_Enoch\Facewear\Data\UI\icon_G_AirPurifyingRespirator_01_F_ca.paa";
+		hiddenSelections[]=
+		{
+			"Camo",
+			"filter"
+		};
+		hiddenSelectionsTextures[]=
+		{
+			"\xdf\retextures\xdf_uniform\facegear\xdf_apr_a.paa"
+		};
+	};
+	class G_XDF_APR_A_NoFilter: G_AirPurifyingRespirator_01_F
+	{
+		author="Radium";
+		scope=2;
+		scopeArsenal=2;
+		scopeCurator=2;
+		displayName="XDF APR Alpha (No Filter)";
+		picture="\A3\Characters_F_Enoch\Facewear\Data\UI\icon_G_AirPurifyingRespirator_01_F_ca.paa";
+		hiddenSelections[]=
+		{
+			"Camo"
+		};
+		hiddenSelectionsTextures[]=
+		{
+			"\xdf\retextures\xdf_uniform\facegear\xdf_apr_a.paa"
+		};
+	};
+	class G_XDF_APR_B: G_AirPurifyingRespirator_02_black_F
+	{
+		author="Radium";
+		scope=2;
+		scopeArsenal=2;
+		scopeCurator=2;
+		displayName="XDF APR Bravo";
+		picture="\A3\Characters_F_Enoch\Facewear\Data\UI\icon_G_AirPurifyingRespirator_02_black_F_ca.paa";
+		hiddenSelections[]=
+		{
+			"Camo",
+			"filter"
+		};
+		hiddenSelectionsTextures[]=
+		{
+			"\xdf\retextures\xdf_uniform\facegear\xdf_apr_b.paa"
+		};
+	};
+	class G_XDF_APR_B_NoFilter: G_AirPurifyingRespirator_02_black_F
+	{
+		author="Radium";
+		scope=2;
+		scopeArsenal=2;
+		scopeCurator=2;
+		displayName="XDF APR Bravo (No Filter)";
+		picture="\A3\Characters_F_Enoch\Facewear\Data\UI\icon_G_AirPurifyingRespirator_02_black_F_ca.paa";
+		hiddenSelections[]=
+		{
+			"Camo"
+		};
+		hiddenSelectionsTextures[]=
+		{
+			"\xdf\retextures\xdf_uniform\facegear\xdf_apr_b.paa"
 		};
 	};
 };
