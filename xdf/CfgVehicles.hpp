8215ca12efca2f011ef6f0aeab4d0116868ee03a
Radiumio <shaianmacleod@gmail.com>
2025-01-06 14:44:34 +0200

    Added XDF Marine Vests & Uniforms (EF CDLC)

--- xdf/CfgVehicles.hpp
+++ xdf/CfgVehicles.hpp
@@ -22310,6 +22310,7 @@ class CfgVehicles
 	class TCGM_Stealth_B_CTRG_Soldier_arid;
 	class B_CBRN_Man_Oversuit_01_MTP_F;
 	class O_V_Soldier_Viper_hex_F;
+	class B_Soldier_base_F;
 	class TCGM_Viper_O_AHex_Soldier;
 	class B_diver_F;
 	class TCGM_F_Diver_B;
@@ -22430,6 +22431,89 @@ class CfgVehicles
 		{
 			"\xdf\retextures\xdf_uniform\uniforms\U_XDF_SP_Suit.paa"
 		};
+	};
+	class B_XDF_Marine_Uniform: B_Soldier_base_F // 
+	{
+		author="Radium";
+		DLC="ef";
+		scope=1;
+		scopeCurator=0;
+		class Wounds
+		{
+			tex[]={};
+			mat[]=
+			{
+				"ef\ef_marines\Data\US_Marines_FROG.rvmat", 
+				"ef\ef_marines\Data\US_Marines_FROG_injury.rvmat", 
+				"ef\ef_marines\Data\US_Marines_FROG_injury.rvmat", 
+				"A3\Characters_F\BLUFOR\Data\clothing1.rvmat", 
+				"A3\Characters_F\BLUFOR\Data\clothing1_injury.rvmat", 
+				"A3\Characters_F\BLUFOR\Data\clothing1_injury.rvmat", 
+				"A3\Characters_F\Common\Data\basicbody.rvmat", 
+				"A3\Characters_F\Common\Data\basicbody_injury.rvmat", 
+				"A3\Characters_F\Common\Data\basicbody_injury.rvmat", 
+				"a3\characters_f\heads\data\hl_white.rvmat", 
+				"a3\characters_f\heads\data\hl_white_injury.rvmat", 
+				"a3\characters_f\heads\data\hl_white_injury.rvmat", 
+				"A3\Characters_F\Heads\Data\hl_white_bald_muscular.rvmat", 
+				"A3\Characters_F\Heads\Data\hl_white_bald_muscular_injury.rvmat", 
+				"A3\Characters_F\Heads\Data\hl_white_bald_muscular_injury.rvmat", 
+				"A3\Characters_F\Heads\Data\hl_white_02_bald_muscular.rvmat", 
+				"A3\Characters_F\Heads\Data\hl_white_02_bald_muscular_injury.rvmat", 
+				"A3\Characters_F\Heads\Data\hl_white_02_bald_muscular_injury.rvmat", 
+				"A3\Characters_F\Heads\Data\hl_black_bald_muscular.rvmat", 
+				"A3\Characters_F\Heads\Data\hl_black_bald_muscular_injury.rvmat", 
+				"A3\Characters_F\Heads\Data\hl_black_bald_muscular_injury.rvmat", 
+				"A3\Characters_F\Heads\Data\hl_white_hairy_muscular.rvmat", 
+				"A3\Characters_F\Heads\Data\hl_white_hairy_muscular_injury.rvmat", 
+				"A3\Characters_F\Heads\Data\hl_white_hairy_muscular_injury.rvmat", 
+				"A3\Characters_F\Heads\Data\hl_white_old.rvmat", 
+				"A3\Characters_F\Heads\Data\hl_white_old_injury.rvmat", 
+				"A3\Characters_F\Heads\Data\hl_white_old_injury.rvmat", 
+				"A3\Characters_F\Heads\Data\hl_asian_bald_muscular.rvmat", 
+				"A3\Characters_F\Heads\Data\hl_asian_bald_muscular_injury.rvmat", 
+				"A3\Characters_F\Heads\Data\hl_asian_bald_muscular_injury.rvmat", 
+				"A3\Characters_F_Exp\Heads\Data\hl_tanoan_bald_muscular.rvmat", 
+				"A3\Characters_F_Exp\Heads\Data\hl_tanoan_bald_muscular_injury.rvmat", 
+				"A3\Characters_F_Exp\Heads\Data\hl_tanoan_bald_muscular_injury.rvmat", 
+				"A3\Characters_F_Exp\Heads\Data\hl_asian_02_bald_muscular.rvmat", 
+				"A3\Characters_F_Exp\Heads\Data\hl_asian_02_bald_muscular_injury.rvmat", 
+				"A3\Characters_F_Exp\Heads\Data\hl_asian_02_bald_muscular_injury.rvmat"
+			};
+		};
+		model="\ef\ef_marines\ef_u_frog.p3d";
+		//modelSides[]={3, 1};
+		//nakedUniform="U_BasicBody";
+		uniformClass="U_XDF_Marine_Uniform";
+		hiddenSelections[]=
+		{
+			"Camo",
+			"insignia"
+		};
+		hiddenSelectionsTextures[]=
+		{
+			"\xdf\retextures\xdf_uniform\uniforms\U_XDF_Marines_co.paa"
+		};
+	};
+	class B_XDF_Marine_Uniform_Gloved: B_XDF_Marine_Uniform
+	{
+		model="\ef\ef_marines\ef_u_frog_gl.p3d";
+	};
+	class B_XDF_Marine_Uniform_Gloved_Pads: B_XDF_Marine_Uniform
+	{
+		model="\ef\ef_marines\ef_u_frog_gl_kp.p3d";
+	};
+	class B_XDF_Marine_Uniform_Gloved_Rolled: B_XDF_Marine_Uniform
+	{
+		model="\ef\ef_marines\ef_u_frog_sl_gl.p3d";
+	};
+	class B_XDF_Marine_Uniform_Gloved_Rolled_Pads: B_XDF_Marine_Uniform
+	{
+		model="\ef\ef_marines\ef_u_frog_sl_gl_kp.p3d";
+	};
+	class B_XDF_Marine_Uniform_Rolled: B_XDF_Marine_Uniform
+	{
+		model="\ef\ef_marines\ef_u_frog_sl.p3d";
 	};
 	class B_XDF_Wetsuit: B_diver_F
 	{
--- xdf/CfgWeapons.hpp
+++ xdf/CfgWeapons.hpp
@@ -41,6 +41,7 @@ class cfgWeapons
 	class TCGM_CombatUniform_CTRG_Bra;
 	class U_B_CBRN_Suit_01_MTP_F;
 	class U_O_V_Soldier_Viper_hex_F;
+	class EF_U_B_MarineCombatUniform_Des_5;
 	class TCGM_F_Viper_GHex;
 	class U_B_Wetsuit;
 	class TCGM_F_Wetsuit_B;
@@ -217,6 +218,85 @@ class cfgWeapons
 		{
 			uniformModel="-";
 			uniformClass="B_XDF_SP_Suit";
+			containerClass="Supply40";
+			mass=40;
+		};
+	};
+	class U_XDF_Marine_Uniform: EF_U_B_MarineCombatUniform_Des_5
+	{
+		author="Radium";
+		DLC="ef";
+		scope=2;
+		scopeArsenal=2;
+		displayName="XDF Marine Combat Uniform";
+		//picture="\SSU_Black_Uniforms\UI\Icon_SSU_Special_Purpose_Suit_Black.paa";
+		hiddenSelections[]=
+		{
+			"camo"
+		};
+		hiddenSelectionsTextures[]=
+		{
+			"\xdf\retextures\xdf_uniform\uniforms\U_XDF_Marines_co.paa"
+		};
+		class ItemInfo: UniformItem
+		{
+			uniformModel="-";
+			uniformClass="B_XDF_Marine_Uniform";
+			containerClass="Supply40";
+			mass=40;
+		};
+	};
+	class U_XDF_Marine_Uniform_Gloved: U_XDF_Marine_Uniform
+	{
+		displayName="XDF Marine Combat Uniform (Gloves)";
+		class ItemInfo: UniformItem
+		{
+			uniformModel="-";
+			uniformClass="B_XDF_Marine_Uniform_Gloved";
+			containerClass="Supply40";
+			mass=40;
+		};
+	};
+	class U_XDF_Marine_Uniform_Gloved_Pads: U_XDF_Marine_Uniform
+	{
+		displayName="XDF Marine Combat Uniform (Gloves/Pads)";
+		class ItemInfo: UniformItem
+		{
+			uniformModel="-";
+			uniformClass="B_XDF_Marine_Uniform_Gloved_Pads";
+			containerClass="Supply40";
+			mass=40;
+		};
+	};
+	class U_XDF_Marine_Uniform_Gloved_Rolled: U_XDF_Marine_Uniform
+	{
+		displayName="XDF Marine Combat Uniform (Gloves/Rolled)";
+		class ItemInfo: UniformItem
+		{
+			uniformModel="-";
+			uniformClass="B_XDF_Marine_Uniform_Gloved_Rolled";
+			containerClass="Supply40";
+			mass=40;
+		};
+	};
+	class U_XDF_Marine_Uniform_Gloved_Rolled_Pads: U_XDF_Marine_Uniform
+	{
+		displayName="XDF Marine Combat Uniform (Gloves/Rolled/Pads)";
+		class ItemInfo: UniformItem
+		{
+			uniformModel="-";
+			uniformClass="B_XDF_Marine_Uniform_Gloved_Rolled_Pads";
+			containerClass="Supply40";
+			mass=40;
+		};
+	};
+	class U_XDF_Marine_Uniform_Rolled: U_XDF_Marine_Uniform
+	{
+		displayName="XDF Marine Combat Uniform (Rolled)";
+		class ItemInfo: UniformItem
+		{
+			uniformModel="-";
+			uniformClass="B_XDF_Marine_Uniform_Rolled";
 			containerClass="Supply40";
 			mass=40;
 		};
@@ -705,6 +785,83 @@ class cfgWeapons
 			};
 		};
 	};
+	class EF_V_AAV_Black;
+	class V_XDF_AAV: EF_V_AAV_Black
+	{
+		author="Radium";
+		scope=2;
+		displayName="XDF Marine Vest";
+		//picture="\SSU_Black_Vests\UI\Icon_SSU_Carrier_Rig_Black.paa";
+		hiddenSelectionsTextures[]=
+		{
+			"\xdf\retextures\xdf_uniform\vests\V_XDF_AA_vest_co.paa"
+		};
+	};
+	class EF_V_AAV_Rifleman_Black;
+	class V_XDF_AAV_Rifleman: EF_V_AAV_Rifleman_Black
+	{
+		author="Radium";
+		scope=2;
+		displayName="XDF Marine Vest (Rifleman)";
+		//picture="\SSU_Black_Vests\UI\Icon_SSU_Carrier_Rig_Black.paa";
+		hiddenSelectionsTextures[]=
+		{
+			"\xdf\retextures\xdf_uniform\vests\V_XDF_AA_vest_co.paa",
+			"\a3\weapons_f_epb\Rifles\MX_Black\Data\XMX_Base_Black_co.paa"
+		};
+	};
+	class EF_V_AAV_Scout_Black;
+	class V_XDF_AAV_Scout: EF_V_AAV_Scout_Black
+	{
+		author="Radium";
+		scope=2;
+		displayName="XDF Marine Vest (Scout)";
+		//picture="\SSU_Black_Vests\UI\Icon_SSU_Carrier_Rig_Black.paa";
+		hiddenSelectionsTextures[]=
+		{
+			"\xdf\retextures\xdf_uniform\vests\V_XDF_AA_vest_co.paa",
+			"\a3\weapons_f_epb\Rifles\MX_Black\Data\XMX_Base_Black_co.paa"
+		};
+	};
+	class EF_V_AAV_Scout_Black;
+	class V_XDF_AAV_Medic: EF_V_AAV_Scout_Black
+	{
+		author="Radium";
+		scope=2;
+		displayName="XDF Marine Vest (Medic)";
+		//picture="\SSU_Black_Vests\UI\Icon_SSU_Carrier_Rig_Black.paa";
+		hiddenSelectionsTextures[]=
+		{
+			"\xdf\retextures\xdf_uniform\vests\V_XDF_AA_med_vest_co.paa",
+			"\a3\weapons_f_epb\Rifles\MX_Black\Data\XMX_Base_Black_co.paa"
+		};
+	};
+	class EF_V_AAV_Support_Black;
+	class V_XDF_AAV_Support: EF_V_AAV_Support_Black
+	{
+		author="Radium";
+		scope=2;
+		displayName="XDF Marine Vest (Support)";
+		//picture="\SSU_Black_Vests\UI\Icon_SSU_Carrier_Rig_Black.paa";
+		hiddenSelectionsTextures[]=
+		{
+			"\xdf\retextures\xdf_uniform\vests\V_XDF_AA_vest_co.paa"
+		};
+	};
+	class EF_V_AAV_TL_Black;
+	class V_XDF_AAV_TL: EF_V_AAV_TL_Black
+	{
+		author="Radium";
+		scope=2;
+		displayName="XDF Marine Vest (Team Leader)";
+		//picture="\SSU_Black_Vests\UI\Icon_SSU_Carrier_Rig_Black.paa";
+		hiddenSelectionsTextures[]=
+		{
+			"\xdf\retextures\xdf_uniform\vests\V_XDF_AA_vest_co.paa",
+			"\a3\weapons_f_epb\Rifles\MX_Black\Data\XMX_Base_Black_co.paa"
+		};
+	};
+
 
 	class H_SSU_Stealth_Combat_Helmet_base;
 	class H_SSU_Combat_Helmet_base;
