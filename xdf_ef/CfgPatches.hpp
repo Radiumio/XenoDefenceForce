3be4422bf92eac89c32ec5180243ad2f9b6a45d4
Radiumio <shaianmacleod@gmail.com>
2025-01-06 16:05:51 +0200

    Split XDF EF assets into compat pbo

---
+++ xdf_ef/CfgPatches.hpp
@@ -0,0 +1,18 @@
+class CfgPatches 
+{
+	class xdf_ef
+	{
+		requiredAddons[] = 
+		{
+			"xdf",
+			"ef"
+		};
+		version = "1.0";
+		author="Radium";
+		authorUrl = "https://steamcommunity.com/id/radiumio/";
+		units[] = {};
+		weapons[] = {};
+		
+		skipWhenMissingDependencies = 1;
+	};
+};
