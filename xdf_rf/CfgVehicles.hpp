5b7e1cabc7b8ca32dd6ec2e7126e877959a34dd7
Radiumio <shaianmacleod@gmail.com>
2025-01-06 17:23:34 +0200

    Base edited Mamba textures

--- xdf_rf/CfgVehicles.hpp
+++ xdf_rf/CfgVehicles.hpp
@@ -1,15 +1,7 @@
 class CfgVehicles
 {
 	class B_Heli_EC_03_RF;
-    class B_Heli_EC_03_RF_OCimport_01 : B_Heli_EC_03_RF { scope = 0; class EventHandlers; class Turrets; };
-    class B_Heli_EC_03_RF_OCimport_02 : B_Heli_EC_03_RF_OCimport_01 { 
-        class EventHandlers; 
-        class Turrets : Turrets {
-			class CopilotTurret;
-            class MainTurret;
-        };
-    };
-	class XDF_Mamba: B_Heli_EC_03_RF_OCimport_02
+	class XDF_Mamba: B_Heli_EC_03_RF
 	{
 		author="Radium";
 		scope = 2;
@@ -19,9 +11,8 @@ class CfgVehicles
 		side = 1;
 		faction = "B_XDF";
 		crew = "B_XDF_Pilot";
-		armor = 120; // Default 40
-		maxSpeed = 300; // Default 300
-		fuelCapacity = 2300; // Default 1360
+		armor = 110; // Default 50
+		fuelCapacity = 1800; // Default 1250
 		damageResistance = 0.02; // Default 0
 		typicalCargo[]=
 		{
@@ -36,16 +27,139 @@ class CfgVehicles
 			"\xdf_rf\retextures\mamba\mh252_mamba_co.paa",
 			"\lxRF\air_rf\heli_medium_ec\data\as332_int_cargo_co.paa"
 		};
-		class Turrets : Turrets
+		class TextureSources
 		{
-			class CopilotTurret : CopilotTurret 
-			{ 
-				gunnerType = "B_XDF_Pilot"; 
+			class XDF
+			{
+				displayName="Xeno Defence Force";
+				author="Radium";
+				factions[]=
+				{
+					"B_XDF"
+				};
+				textures[]=
+				{
+					"\xdf_rf\retextures\mamba\mh252_mamba_co.paa",
+					"\lxRF\air_rf\heli_medium_ec\data\as332_int_cargo_co.paa",
+					"#(rgb,1024,1024,1)ui('lxRF_MFDMinimap','lxRF_MFDMinimap')",
+					"\xdf_rf\retextures\mamba\mh252_mamba_adds_co.paa",
+					"\xdf_rf\retextures\mamba\mh252_mamba_co.paa",
+					"\lxRF\air_rf\heli_medium_ec\data\as332_int_cargo_co.paa"
+				};
 			};
-            class MainTurret : MainTurret 
-			{ 
-				gunnerType = "B_XDF_Heli_Crew"; 
+		};
+		textureList[]=
+		{
+			"XDF",
+			1
+		};
+	};
+	class B_Heli_EC_04_military_RF;
+	class XDF_Mamba_Unarmed: B_Heli_EC_04_military_RF
+	{
+		author="Radium";
+		scope = 2;
+		scopeCurator = 2;
+		displayName = "XDF MH-252 Mamba (Unarmed)";
+		editorPreview="\xdf\previews\vehicles\XDF_Mamba_Unarmed.jpg";
+		side = 1;
+		faction = "B_XDF";
+		crew = "B_XDF_Pilot";
+		armor = 110; // Default 50
+		fuelCapacity = 1800; // Default 1250
+		damageResistance = 0.02; // Default 0
+		typicalCargo[]=
+		{
+			"B_XDF_Pilot"
+		};
+		hiddenSelectionsTextures[]=
+		{
+			"\xdf_rf\retextures\mamba\mh252_mamba_co.paa",
+			"\lxRF\air_rf\heli_medium_ec\data\as332_int_cargo_co.paa",
+			"#(rgb,1024,1024,1)ui('lxRF_MFDMinimap','lxRF_MFDMinimap')",
+			"\xdf_rf\retextures\mamba\mh252_mamba_adds_co.paa",
+			"\xdf_rf\retextures\mamba\mh252_mamba_co.paa",
+			"\lxRF\air_rf\heli_medium_ec\data\as332_int_cargo_co.paa"
+		};
+		class TextureSources
+		{
+			class XDF
+			{
+				displayName="Xeno Defence Force";
+				author="Radium";
+				factions[]=
+				{
+					"B_XDF"
+				};
+				textures[]=
+				{
+					"\xdf_rf\retextures\mamba\mh252_mamba_co.paa",
+					"\lxRF\air_rf\heli_medium_ec\data\as332_int_cargo_co.paa",
+					"#(rgb,1024,1024,1)ui('lxRF_MFDMinimap','lxRF_MFDMinimap')",
+					"\xdf_rf\retextures\mamba\mh252_mamba_adds_co.paa",
+					"\xdf_rf\retextures\mamba\mh252_mamba_co.paa",
+					"\lxRF\air_rf\heli_medium_ec\data\as332_int_cargo_co.paa"
+				};
 			};
+		};
+		textureList[]=
+		{
+			"XDF",
+			1
+		};
+	};
+	class I_Heli_EC_02_RF;
+	class XDF_Mamba_SOCAT: I_Heli_EC_02_RF
+	{
+		author="Radium";
+		scope = 2;
+		scopeCurator = 2;
+		displayName = "XDF MH-252 Mamba SOCAT";
+		editorPreview="\xdf\previews\vehicles\XDF_Mamba_SOCAT.jpg";
+		side = 1;
+		faction = "B_XDF";
+		crew = "B_XDF_Pilot";
+		armor = 110; // Default 50
+		fuelCapacity = 1800; // Default 1250
+		damageResistance = 0.02; // Default 0
+		typicalCargo[]=
+		{
+			"B_XDF_Pilot"
+		};
+		hiddenSelectionsTextures[]=
+		{
+			"\xdf_rf\retextures\mamba\mh252_mamba_co.paa",
+			"\lxRF\air_rf\heli_medium_ec\data\as332_int_cargo_co.paa",
+			"#(rgb,1024,1024,1)ui('lxRF_MFDMinimap','lxRF_MFDMinimap')",
+			"\xdf_rf\retextures\mamba\mh252_mamba_adds_co.paa",
+			"\xdf_rf\retextures\mamba\mh252_mamba_co.paa",
+			"\lxRF\air_rf\heli_medium_ec\data\as332_int_cargo_co.paa"
+		};
+		class TextureSources
+		{
+			class XDF
+			{
+				displayName="Xeno Defence Force";
+				author="Radium";
+				factions[]=
+				{
+					"B_XDF"
+				};
+				textures[]=
+				{
+					"\xdf_rf\retextures\mamba\mh252_mamba_co.paa",
+					"\lxRF\air_rf\heli_medium_ec\data\as332_int_cargo_co.paa",
+					"#(rgb,1024,1024,1)ui('lxRF_MFDMinimap','lxRF_MFDMinimap')",
+					"\xdf_rf\retextures\mamba\mh252_mamba_adds_co.paa",
+					"\xdf_rf\retextures\mamba\mh252_mamba_co.paa",
+					"\lxRF\air_rf\heli_medium_ec\data\as332_int_cargo_co.paa"
+				};
+			};
+		};
+		textureList[]=
+		{
+			"XDF",
+			1
 		};
 	};
 };
