45757a1c7a83f777b8ec13fe0cf09b88cfc22f85
Radiumio <shaianmacleod@gmail.com>
2025-01-13 02:30:54 +0200

    Add various XDF Ballistic Masks

---
+++ xdf_ws/CfgWeapons.hpp
@@ -0,0 +1,143 @@
+class CfgWeapons
+{
+	class lxWS_H_bmask_base;
+	class H_XDF_Ballistic_Mask_Plain: lxWS_H_bmask_base
+	{
+		author="Radium";
+		scope=2;
+		displayName="XDF Ballistic Mask (Plain)";
+		hiddenSelectionsTextures[] = 
+		{
+			"\xdf_ws\retextures\xdf_uniform\helmets\ballistic_mask_co.paa"
+		};
+	};
+	
+	class H_turban_02_mask_black_lxws;
+	class H_XDF_Ballistic_Mask_Plain_Shemagh: H_turban_02_mask_black_lxws
+	{
+		author="Radium";
+		scope=2;
+		displayName="XDF Ballistic Mask (Plain, Shemagh)";
+		hiddenSelectionsTextures[] = 
+		{
+			"\xdf_ws\retextures\xdf_uniform\helmets\turban_black_co.paa",
+			"\xdf_ws\retextures\xdf_uniform\helmets\ballistic_mask_co.paa"
+		};
+	};
+	class lxWS_H_bmask_base;
+	class H_XDF_Ballistic_Mask: lxWS_H_bmask_base
+	{
+		author="Radium";
+		scope=2;
+		displayName="XDF Ballistic Mask (XDF)";
+		hiddenSelectionsTextures[] = 
+		{
+			"\xdf_ws\retextures\xdf_uniform\helmets\ballistic_mask_xdf_co.paa"
+		};
+	};
+	
+	class H_turban_02_mask_black_lxws;
+	class H_XDF_Ballistic_Mask_Shemagh: H_turban_02_mask_black_lxws
+	{
+		author="Radium";
+		scope=2;
+		displayName="XDF Ballistic Mask (XDF, Shemagh)";
+		hiddenSelectionsTextures[] = 
+		{
+			"\xdf_ws\retextures\xdf_uniform\helmets\turban_black_co.paa",
+			"\xdf_ws\retextures\xdf_uniform\helmets\ballistic_mask_xdf_co.paa"
+		};
+	};
+	class lxWS_H_bmask_base;
+	class H_XDF_Ballistic_Mask_Beast: lxWS_H_bmask_base
+	{
+		author="Radium";
+		scope=2;
+		displayName="XDF Ballistic Mask (Beast)";
+		hiddenSelectionsTextures[] = 
+		{
+			"\xdf_ws\retextures\xdf_uniform\helmets\ballistic_mask_beast_co.paa"
+		};
+	};
+	class H_turban_02_mask_black_lxws;
+	class H_XDF_Ballistic_Mask_Beast_Shemagh: H_turban_02_mask_black_lxws
+	{
+		author="Radium";
+		scope=2;
+		displayName="XDF Ballistic Mask (Beast, Shemagh)";
+		hiddenSelectionsTextures[] = 
+		{
+			"\xdf_ws\retextures\xdf_uniform\helmets\turban_black_co.paa",
+			"\xdf_ws\retextures\xdf_uniform\helmets\ballistic_mask_beast_co.paa"
+		};
+	};
+	class lxWS_H_bmask_base;
+	class H_XDF_Ballistic_Mask_Marine: lxWS_H_bmask_base
+	{
+		author="Radium";
+		scope=2;
+		displayName="XDF Ballistic Mask (Marine)";
+		hiddenSelectionsTextures[] = 
+		{
+			"\xdf_ws\retextures\xdf_uniform\helmets\ballistic_mask_marine_co.paa"
+		};
+	};
+	class H_turban_02_mask_black_lxws;
+	class H_XDF_Ballistic_Mask_Marine_Shemagh: H_turban_02_mask_black_lxws
+	{
+		author="Radium";
+		scope=2;
+		displayName="XDF Ballistic Mask (Marine, Shemagh)";
+		hiddenSelectionsTextures[] = 
+		{
+			"\xdf_ws\retextures\xdf_uniform\helmets\turban_black_co.paa",
+			"\xdf_ws\retextures\xdf_uniform\helmets\ballistic_mask_marine_co.paa"
+		};
+	};
+	class lxWS_H_bmask_base;
+	class H_XDF_Ballistic_Mask_Dots: lxWS_H_bmask_base
+	{
+		author="Radium";
+		scope=2;
+		displayName="XDF Ballistic Mask (Dots)";
+		hiddenSelectionsTextures[] = 
+		{
+			"\xdf_ws\retextures\xdf_uniform\helmets\ballistic_mask_dots_co.paa"
+		};
+	};
+	class H_turban_02_mask_black_lxws;
+	class H_XDF_Ballistic_Mask_Dots_Shemagh: H_turban_02_mask_black_lxws
+	{
+		author="Radium";
+		scope=2;
+		displayName="XDF Ballistic Mask (Dots, Shemagh)";
+		hiddenSelectionsTextures[] = 
+		{
+			"\xdf_ws\retextures\xdf_uniform\helmets\turban_black_co.paa",
+			"\xdf_ws\retextures\xdf_uniform\helmets\ballistic_mask_dots_co.paa"
+		};
+	};
+	class lxWS_H_bmask_base;
+	class H_XDF_Ballistic_Mask_XDF_Dots: lxWS_H_bmask_base
+	{
+		author="Radium";
+		scope=2;
+		displayName="XDF Ballistic Mask (XDF Dots)";
+		hiddenSelectionsTextures[] = 
+		{
+			"\xdf_ws\retextures\xdf_uniform\helmets\ballistic_mask_xdf_dots_co.paa"
+		};
+	};
+	class H_turban_02_mask_black_lxws;
+	class H_XDF_Ballistic_Mask_XDF_Dots_Shemagh: H_turban_02_mask_black_lxws
+	{
+		author="Radium";
+		scope=2;
+		displayName="XDF Ballistic Mask (XDF Dots, Shemagh)";
+		hiddenSelectionsTextures[] = 
+		{
+			"\xdf_ws\retextures\xdf_uniform\helmets\turban_black_co.paa",
+			"\xdf_ws\retextures\xdf_uniform\helmets\ballistic_mask_xdf_dots_co.paa"
+		};
+	};
+};
