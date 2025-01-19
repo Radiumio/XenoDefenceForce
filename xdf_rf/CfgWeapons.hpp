acc8ef0da9f361445bc06a6f7612e8bb367beafd
Radiumio <shaianmacleod@gmail.com>
2025-01-19 20:35:33 +0200

    Added XDF PSRL-1 PWS (RF CDLC)

--- xdf_rf/CfgWeapons.hpp
+++ xdf_rf/CfgWeapons.hpp
@@ -15,7 +15,19 @@ class CfgWeapons
 		{
 			"\xdf\retextures\xdf_uniform\helmets\H_XDF_Combat_Helmet_Black.paa",
 			"\xdf_rf\retextures\xdf_uniform\helmets\mitznefet_xdf_co.paa"
+		};
+	};
+	class launch_PSRL1_PWS_black_RF;
+	class launch_PSRL1_PWS_xdf_RF: launch_PSRL1_PWS_black_RF
+	{
+		author="Radium";
+		scope=2;
+		displayName="XDF PSRL-1 PWS";
+		baseWeapon="launch_PSRL1_PWS_xdf_RF";
+		hiddenSelectionsTextures[] = 
+		{
+			"\xdf_rf\retextures\xdf_uniform\weapons\launch_PSRL1_01_xdf_CO.paa",
+			"\xdf_rf\retextures\xdf_uniform\weapons\launch_PSRL1_02_xdf_CO.paa"
 		};	
-		// "\A3\Characters_F_EPA\BLUFOR\Data\equip1_grass_co.paa", 
 	};
 };
