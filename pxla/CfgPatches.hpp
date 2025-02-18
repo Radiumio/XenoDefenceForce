5100f3cf4f72e77984dc6ed16e9572d2e25f0159
Radiumio <shaianmacleod@gmail.com>
2025-02-18 05:35:02 +0200

    PXLA CfgPatches

--- pxla/CfgPatches.hpp
+++ pxla/CfgPatches.hpp
@@ -12,11 +12,26 @@ class CfgPatches
 		authorUrl = "https://steamcommunity.com/id/radiumio/";
 		units[] = 
 		{
-			""
+			"PXLA_Dementer",
+			"PXLA_Offroad",
+			"PXLA_Offroad_AT",
+			"PXLA_Offroad_HMG",
+			"O_PXLA_Grunt",
+			"O_PXLA_Crewman",
+			"O_PXLA_Grenadier",
+			"O_PXLA_Machinegunner",
+			"O_PXLA_HeavyGunner",
+			"O_PXLA_Marksman",
+			"O_PXLA_Medic",
+			"O_PXLA_Defector",
+			"O_PXLA_Infiltrator",
+			"O_PXLA_Rifleman"
 		};
 		weapons[] = 
 		{
-			""
+			"U_PXLA_Uniform_Red",
+			"V_PXLA_Defector_Carrier_Lite_Red",
+			"V_XLA_Defector_Carrier_Rig_Red"
 		};
 	};
 
