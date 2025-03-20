0f2517c74303e991caa4cbf038eb3dd63570e76e
Radiumio <shaianmacleod@gmail.com>
2025-03-20 21:40:00 +0200

    Update CfgWeapons.hpp

--- xdf_ef/CfgWeapons.hpp
+++ xdf_ef/CfgWeapons.hpp
@@ -158,6 +158,21 @@ class cfgWeapons
 			containerClass="Supply200";
 		};
 	};
+	class V_XDF_AAV_Scout_Maple: EF_V_AAV_Scout_Black
+	{
+		author="Radium";
+		scope=2;
+		displayName="XDF Marine Vest (Scout, Maple)";
+		hiddenSelectionsTextures[]=
+		{
+			"\xdf_ef\retextures\xdf_uniform\vests\V_XDF_AA_vest_co_Maple.paa",
+			"\a3\weapons_f_epb\Rifles\MX_Black\Data\XMX_Base_Black_co.paa"
+		};
+		class ItemInfo: ItemInfo
+		{
+			containerClass="Supply200";
+		};
+	};
 	class EF_V_AAV_Scout_Black;
 	class V_XDF_AAV_Medic: EF_V_AAV_Scout_Black
 	{
