348efa75810237d236e10765054e32d531a6806a
Radiumio <shaianmacleod@gmail.com>
2025-08-12 13:02:11 +0200

    pain

--- xdf/CfgVehicles.hpp
+++ xdf/CfgVehicles.hpp
@@ -24944,7 +24944,27 @@ class CfgVehicles
 	///////////////////////////
 
 	class Helicopter;
-	class XDF_Nemesis: Helicopter
+	class Helicopter_Base_F: Helicopter
+	{
+		class Turrets;
+	};
+
+	class Helicopter_Base_H: Helicopter_Base_F
+	{
+		class Turrets: Turrets
+		{
+			class CopilotTurret;
+		};
+		class AnimationSources;
+		class Eventhandlers;
+		class Viewoptics;
+		class ViewPilot;
+		class Reflectors
+		{
+			class Right;
+		};
+	};
+	class XDF_Nemesis: Helicopter_Base_H
 	{
 		author="Radium";
 		scope=2;
@@ -25189,6 +25209,131 @@ class CfgVehicles
 		gearRetracting = true;
 		gearUpTime = 7;
 		gearDownTime = 7;
+		class Turrets: Turrets										/// just a copilot seat as a turret to enable taking the controls
+		{
+			class CopilotTurret: CopilotTurret						/// taking controls is already defined in parent class
+			{
+				body = "mainTurret";
+				gun = "weaponry";
+				gunnerAction = Pilot_Plane_Fighter_01;				/// what action does copilot switch to
+				gunnerInAction = Pilot_Plane_Fighter_01;				/// what action does copilot switch to
+				precisegetinout = 1;								/// describes what style of get in is used (0 - non-precise; 1 - precise on proxy; 2 - precise on model center)
+				gunnerGetInAction = copilot_Heli_Light_02_Enter;	/// what action uses the copilot to get in the heli, it uses "switchAction" script command on the proxy
+				gunnerGetOutAction = copilot_Heli_Light_02_Exit;	/// what action uses the copilot to get out the heli				
+				memoryPointsGetInCargo = "pos gunner";				/// what is the position of get in action
+				memoryPointsGetInCargoDir = "pos gunner dir";		/// what is the direction of get in action
+				canEject = 0;										/// copilot shouldn't be able to do so as he doesn't have eject seat
+				minElev = 0; 										/// what is the lowest possible elevation of the turret
+				maxElev = +10;  									/// what is the highest possible elevation of the turret
+				initElev = 0; 										/// what is the starting elevation of the turret
+				minTurn = 360;  									/// what is the right-most possible turn of the turret
+				maxTurn = 360;  									/// what is the left-most possible turn of the turret
+				initTurn = 0; 										/// what is the default horizontal turn of the turret
+				maxHorizontalRotSpeed = 3;							/// how fast is the copilot able to look around (higher means faster)
+				maxVerticalRotSpeed = 3;							/// how fast is the copilot able to look around (higher means faster)
+			};
+		};
+		class AnimationSources: AnimationSources	/// custom made animation sources
+		{
+			class Doors				/// the class name is later used in model.cfg
+			{
+				source = door;		/// door source means it is used by animateDoor script command
+				animPeriod = 1;		/// how long does it take to change value from 0 to 1 (or vice versa)
+				initPhase = 0;		/// what value does it have while creating the vehicle
+			};
+			class Nemesis_Minigun
+			{
+				source = "revolving";			/// returns bullets left in magazine of said weapon
+				weapon = "Nemesis_AX_Minigun";	/// this is the said weapon
+			};
+			class Nemesis_Minigun_Flash
+			{
+				source = "ammorandom";			/// this source returns some random value changine each time ammo is spent from this weapon - used for muzzle flashes
+				weapon = "Nemesis_AX_Minigun";	/// this is the said weapon
+			};	
+			class Nemesis_Autocannon
+			{
+				source = "revolving";			/// returns bullets left in magazine of said weapon
+				weapon = "Nemesis_AX_Autocannon";	/// this is the said weapon
+			};
+			class Nemesis_Autocannon_Flash
+			{
+				source = "ammorandom";			/// this source returns some random value changine each time ammo is spent from this weapon - used for muzzle flashes
+				weapon = "Nemesis_AX_Autocannon";	/// this is the said weapon
+			};					
+		};	
+		
+		class UserActions /// actions available for player to interact with vehicle via action menu running scripts
+		{
+			class Door_Open
+			{
+				userActionID 		= 60;				/// ID for some scripts
+				displayName 		= "Open door";		/// what is displayed in action menu
+				displayNameDefault 	= "<img image='\A3\Ui_f\data\IGUI\Cfg\Actions\open_door_ca.paa' size='2.5' />"; /// what is displayed under the cursor (icon in this case)
+				position 			= "";				/// start of radius where action is available
+				condition 			= "this doorPhase ""Doors"" < 0.5 AND Alive(this) AND (player in crew this)"; /// only openable from inside and when closed
+				statement 			= "this animateDoor ['Doors', 1]";	/// sets animation source Doors to 1 via interpolation
+				priority 			= 1.5;				/// higher priority means higher in the Action menu
+				radius 				= 1.5;				/// how far from position is the action available
+				showWindow 			= 0;				/// 0 means that it is not a default action when entering the vehicle
+				onlyForPlayer 		= 1;				/// AI doesn't use this one
+				shortcut 			= "";				/// there's no shortcut for this action
+			};
+
+			class Door_Close: Door_Open
+			{
+				userActionID 		= 61;
+				displayName 		= "Close door";
+				textToolTip 		= "Close door";
+				condition	 		= "this doorPhase ""Doors"" > 0.5 AND Alive(this) AND (player in crew this)";
+				statement 			= "this animateDoor ['Doors', 0]";
+			};
+		};	
+
+		class Reflectors: Reflectors			/// landing lights of the heli, turned on by AI while in night and "careless" or "safe"
+		{
+			class Right
+			{
+				color[] = {7000,7500,10000};	/// defines red, green, blue and alpha components of the light
+				ambient[] = {70,75,100};		/// the same definition format for colouring the environment around
+				intensity = 50;					/// how much does the light shine (in some strange units, just tweak until it is satisfying), rough approximation is intensity = (brightness * 50) ^ 2
+				size = 1;						/// defines the visible size of light, has not much of an effect now
+				innerAngle = 15;				/// angle from light direction vector where the light is at full strength
+				outerAngle = 65;				/// angle from light direction vector where the light is completely faded out
+				coneFadeCoef = 10;				/// coefficient of fading the light between inner and outer cone angles
+
+				position = "Light_R_pos";		/// name of memory point in model to take the origin of the light
+				direction = "Light_R_dir";		/// name of memory point in the model to make a vector of direction of light from it's position
+				hitpoint = "Light_R_hitpoint";	/// name of hitpoint selection in hitpoint lod of the model to be affected by damage
+				selection = "Light_R";			/// name of selection in visual lods of the model that are going to be hidden while the light is off
+
+				useFlare = true;				/// boolean switch if the light produces flare or not
+				flareSize = 10;					/// how big is the flare, using the same metrics as intensity
+				flareMaxDistance = 250;			/// maximum distance where the flare is drawn
+
+				dayLight = false;				/// boolean switch if the light is used during day or not
+
+				class Attenuation
+				{
+					start = 0;					/// offset of start of the attenuation
+					constant = 0;				/// constant attenuation of the light in any distance from source
+					linear = 1;					/// coefficient for linear attenuation
+					quadratic = 1;				/// coefficient for attenuation with square of distance from source
+
+					hardLimitStart = 100;		/// distance from source where the light intensity decreases for drawing
+					hardLimitEnd = 200;			/// distance from source where the light is not displayed (shorter distances increase performance)
+				};
+			};
+
+			class Left: Right
+			{
+				position = "Light_L_pos";
+				direction = "Light_L_dir";
+				hitpoint = "Light_L_hitpoint";
+				selection = "Light_L";
+			};			
+		};	
+		aggregateReflectors[] = {{"Left", "Right"}};	/// aggregates both sources into one to increase performance		
 	};
 	class B_UGV_01_rcws_F;
 	class XDF_Mars: B_UGV_01_rcws_F
