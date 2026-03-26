c59bf9bbd1f37df1caeb149034d949511b8a8283
Radiumio <shaianmacleod@gmail.com>
2026-03-26 19:13:06 +0200

    [REDACTED]

---
+++ extraterrestrials/CfgPatches.hpp
@@ -0,0 +1,18 @@
+class CfgPatches 
+{
+	class xdf_extraterrestrials
+	{
+		addonRootClass = "xdf";
+		requiredAddons[] = 
+		{
+			"xdf"
+		};
+		version = "1.0";
+		units[] = 
+		{
+			"Extraterrestrial_Mothership_F"
+		};
+		weapons[] = {};
+		skipWhenMissingDependencies = 1;
+	};
+};
---
+++ extraterrestrials/CfgVehicles.hpp
@@ -0,0 +1,13 @@
+class CfgVehicles
+{
+	class House_F;
+	class Extraterrestrial_Mothership_F: House_F
+	{
+		author="Radium";
+		editorPreview="";
+		scope=2;
+		scopeCurator=2;
+		model="\extraterrestrials\models\alien_mothership\extraterrestrial_mothership.p3d";
+		displayName="Extraterrestrial Mothership";
+	};
+};
