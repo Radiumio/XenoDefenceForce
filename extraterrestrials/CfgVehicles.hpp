8faa428031ecae21cfe856f024e4a964d2afc1ed
Radiumio <shaianmacleod@gmail.com>
2026-03-26 20:54:13 +0200

    Update CfgVehicles.hpp

--- extraterrestrials/CfgVehicles.hpp
+++ extraterrestrials/CfgVehicles.hpp
@@ -7,10 +7,12 @@ class CfgVehicles
 		editorPreview="\xdf\previews\structures\et_preview.jpg";
 		scope=2;
 		scopeCurator=2;
+		armor=999999999999;
 		model="\extraterrestrials\models\alien_mothership\extraterrestrial_mothership.p3d";
 		displayName="Extraterrestrial Mothership";
 		editorCategory="ET_Technology";
 		editorSubcategory="ET_Vessels";
 		icon="\extraterrestrials\models\alien_mothership\et_vessel_icon.paa";
+		class DestructionEffects {};
 	};
 };
