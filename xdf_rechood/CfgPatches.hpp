7484081646bcd90ba18ca0b87a915c9c96baa8b5
Radiumio <shaianmacleod@gmail.com>
2026-04-06 17:35:52 +0200

    Added initial Recon Hoods compat

---
+++ xdf_rechood/CfgPatches.hpp
@@ -0,0 +1,16 @@
+class CfgPatches 
+{
+	class xdf_rechood
+	{
+		addonRootClass = "xdf";
+		requiredAddons[] = 
+		{
+			"xdf",
+			"ReconCloaks_Characters_Headgear"
+		};
+		version = "1.0";
+		units[] = {};
+		weapons[] = {};
+		skipWhenMissingDependencies = 1;
+	};
+};
---
+++ xdf_rechood/CfgWeapons.hpp
@@ -0,0 +1,71 @@
+class CfgWeapons
+{
+	class ReconCloaks_NV_Hood_Down_blk;
+	class ReconCloaks_NV_Hood_Down_XDF: ReconCloaks_NV_Hood_Down_blk
+	{
+		author = "OokamiJamie & Radium";
+		displayName = "XDF Recon Hood (Down)";
+		hiddenSelectionsTextures[] = 
+		{
+			"\xdf_rechood\retextures\xdf_uniform\helmets\H_ReconCloak_XDF_CO.paa";
+		};
+	};
+	class ReconCloaks_H_MaskingHood_dazzle;
+	class ReconCloaks_H_MaskingHood_XDF: ReconCloaks_H_MaskingHood_dazzle
+	{
+		author = "OokamiJamie & Radium";
+		scope = 2;
+		displayName = "XDF Masking Hood";
+		hiddenSelectionsTextures[] = 
+		{
+			"\xdf_rechood\retextures\xdf_uniform\helmets\H_ReconCloak_masking_XDF_CO.paa"
+		};
+	};
+	class ReconCloaks_H_Hood_blk;
+	class ReconCloaks_H_Hood_XDF: ReconCloaks_H_Hood_blk
+	{
+		author = "OokamiJamie & Radium";
+		scope = 2;
+		displayName = "XDF Recon Hood";
+		hiddenSelectionsTextures[] = 
+		{
+			"\xdf_rechood\retextures\xdf_uniform\helmets\H_ReconCloak_XDF_CO.paa"
+		};
+	};
+	class ReconCloaks_H_Hood_Cap_oli;
+	class ReconCloaks_H_Hood_Cap_XDF: ReconCloaks_H_Hood_Cap_oli
+	{
+		author = "OokamiJamie & Radium";
+		scope = 2;
+		displayName = "XDF Recon Hood (Cap)";
+		hiddenSelectionsTextures[] = 
+		{
+			"\xdf_rechood\retextures\xdf_uniform\helmets\H_ReconCloak_XDF_CO.paa",
+			"\xdf_rechood\retextures\xdf_uniform\helmets\H_PatrolCap_XDF_CO.paa"
+		};
+	};
+	class ReconCloaks_H_SniperHood_tna;
+	class ReconCloaks_H_SniperHood_XDF: ReconCloaks_H_SniperHood_tna
+	{
+		author = "OokamiJamie & Radium";
+		scope = 2;
+		displayName = "XDF Sniper Hood";
+		hiddenSelectionsTextures[] = 
+		{
+			"\xdf_rechood\retextures\xdf_uniform\helmets\H_ReconCloak_XDF_CO.paa",
+			"\xdf_rechood\retextures\xdf_uniform\helmets\ghillie_threads_XDF_CA.paa"
+		};
+	};
+	class ReconCloaks_H_SniperHoodSpec_tna;
+	class ReconCloaks_H_SniperHoodSpec_XDF: ReconCloaks_H_SniperHoodSpec_tna
+	{
+		author = "OokamiJamie & Radium";
+		scope = 2;
+		displayName = "XDF Sniper Hood (Scrim)";
+		hiddenSelectionsTextures[] = 
+		{
+			"\xdf_rechood\retextures\xdf_uniform\helmets\H_ReconCloak_XDF_CO.paa",
+			"\xdf_rechood\retextures\xdf_uniform\helmets\ghillie_leaves_XDF_CA.paa"
+		};
+	};
+};
